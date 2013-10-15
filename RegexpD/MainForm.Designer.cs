b81e65f9a76e2fe7b48a7c42edb6aa9e077ee447
Michael Voronoff <michael.voronov@gmail.com>
2013-10-15 14:19:11 +0400

    initial

---
+++ RegexpD/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace RegexpD
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.teInput = new System.Windows.Forms.TextBox();
+			this.tePattern = new System.Windows.Forms.TextBox();
+			this.teResult = new System.Windows.Forms.TextBox();
+			this.cheIgnoreCase = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// teInput
+			// 
+			this.teInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.teInput.Location = new System.Drawing.Point(12, 12);
+			this.teInput.Multiline = true;
+			this.teInput.Name = "teInput";
+			this.teInput.Size = new System.Drawing.Size(884, 149);
+			this.teInput.TabIndex = 0;
+			// 
+			// tePattern
+			// 
+			this.tePattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tePattern.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.tePattern.Location = new System.Drawing.Point(12, 167);
+			this.tePattern.Name = "tePattern";
+			this.tePattern.Size = new System.Drawing.Size(795, 23);
+			this.tePattern.TabIndex = 1;
+			this.tePattern.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tePattern_KeyPress);
+			// 
+			// teResult
+			// 
+			this.teResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.teResult.Location = new System.Drawing.Point(12, 193);
+			this.teResult.Multiline = true;
+			this.teResult.Name = "teResult";
+			this.teResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.teResult.Size = new System.Drawing.Size(884, 395);
+			this.teResult.TabIndex = 2;
+			// 
+			// cheIgnoreCase
+			// 
+			this.cheIgnoreCase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cheIgnoreCase.AutoSize = true;
+			this.cheIgnoreCase.Location = new System.Drawing.Point(813, 169);
+			this.cheIgnoreCase.Name = "cheIgnoreCase";
+			this.cheIgnoreCase.Size = new System.Drawing.Size(83, 17);
+			this.cheIgnoreCase.TabIndex = 3;
+			this.cheIgnoreCase.Text = "Ignore Case";
+			this.cheIgnoreCase.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(908, 600);
+			this.Controls.Add(this.cheIgnoreCase);
+			this.Controls.Add(this.teResult);
+			this.Controls.Add(this.tePattern);
+			this.Controls.Add(this.teInput);
+			this.Name = "MainForm";
+			this.Text = "RegEXpTest";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox teInput;
+		private System.Windows.Forms.TextBox tePattern;
+		private System.Windows.Forms.TextBox teResult;
+		private System.Windows.Forms.CheckBox cheIgnoreCase;
+	}
+}
+
---
+++ RegexpD/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegexpD
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void tePattern_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				Analyze();
+				e.Handled = true;
+			}
+		}
+
+		public void Analyze()
+		{
+			try
+			{
+				List<string> result = new List<string>();
+
+				bool isMatch = Regex.IsMatch(teInput.Text, tePattern.Text, cheIgnoreCase.Checked ? RegexOptions.IgnoreCase : RegexOptions.None);
+
+				result.Add(String.Format("IsMatch: {0}", isMatch));
+				result.Add(String.Empty);
+				result.Add(String.Empty);
+
+				MatchCollection collection = Regex.Matches(teInput.Text, tePattern.Text, cheIgnoreCase.Checked ? RegexOptions.IgnoreCase : RegexOptions.None);
+
+				for (int i = 0; i < collection.Count; i++)
+				{
+					var m = collection[i];
+
+					result.Add(String.Format("#{0}:\tValue: '{1}'", i, m.Value));
+					for (int k = 1; k < m.Groups.Count; k++)
+					{
+						var g = m.Groups[k];
+						result.Add(String.Format("\t\t G#{0}:\tValue: '{1}'", k, g.Value));
+					}
+					result.Add(String.Empty);
+					result.Add(String.Empty);
+				}
+
+				teResult.Lines = result.ToArray();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
