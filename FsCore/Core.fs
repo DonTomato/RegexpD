ca09191ac4448319d2d0b64696735273d40b19ad
Michael Voronoff <michael.voronov@gmail.com>
2013-10-20 11:20:32 +0400

    match in f#

---
+++ FsCore/Core.fs
@@ -0,0 +1,15 @@
+﻿module Core
+
+open System.Text
+open System.Text.RegularExpressions
+
+
+let (|Match|_|) pat inp =
+    let m = Regex.Match(inp, pat)
+    match m.Success with
+    | true -> Some (List.tail [ for g in m.Groups -> g.Value ])
+    | _ -> None
+
+//let getResult x pattern ignoreCase =
+//    match x with
+//    | 
