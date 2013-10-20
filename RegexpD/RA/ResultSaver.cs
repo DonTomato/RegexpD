9b0e33a88597339201015aaf9112533910ff6e2d
Michael Voronoff <michael.voronov@gmail.com>
2013-10-20 11:36:14 +0400

    changes

---
+++ RegexpD/RA/ResultSaver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RegexpD.RA
+{
+    public class ResultSaver
+    {
+        public void Save(List<string> list)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Archieve");
+
+            var archieveDir = new DirectoryInfo(path);
+            if (!archieveDir.Exists)
+                archieveDir.Create();
+        }
+    }
+}
